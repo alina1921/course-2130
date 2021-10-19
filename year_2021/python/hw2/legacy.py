a3f0a54a81c50135edc3d0c39362b72e6ab7e71b
alina1921 <92743460+alina1921@users.noreply.github.com>
2021-10-19 11:47:31 +0300

    Ivanova Alina

--- year_2021/python/hw2/legacy.py
+++ year_2021/python/hw2/legacy.py
@@ -4,34 +4,57 @@ def t1(number):
     Пример: number=21 тогда нужно вернуть 40
     Пример: -5 -> 0
     """
-    pass
-
-
+    while number%20!=0:
+        number=number+1
+    print (number)
 def t2(string):
     """
     На вход подается набор слов, разделенных пробелом, инвертируйте каждое слово.
     Пример: `abc abc abc` -> `cba cba cba`
     """
-    pass
-
-
+    split_str = string.split (' ')
+    list = []
+    for element in split_str:
+        list.append (element [::-1])
+    str='{0}'.format(list)
+    a=str.replace ("[","")
+    a=a.replace ("]","")
+    print (a)
 def t3(dictionary):
     """
     На вход подается словарь. Преобразуйте его в строку по следующему шаблону 'key: value; key: value' и так далее
     """
-    pass
-
-
+    dict='{0}'.format(dictionary)
+    dict1=dict.replace(',',';')
+    b=dict1.replace("'","")
+    b=b.replace('{',"'")
+    b=b.replace('}',"'")
+    print(b)
 def t4(string, sub_string):
     """
     проверить есть ли в строке инвертированная подстрока
     """
-    pass
-
-
-def t5(strings):
-    """
-    На вход подается список строк,
-    Отфильтруйте список строк, оставив только строки в формате: `x y z x*y*z`, где x,y,z - целые положительные числа
-    """
-    pass
+    split_str = string.split (' ')
+    list = []
+    for element in split_str:
+        list.append (element [::-1])
+    str='{0}'.format(list)
+    a=str.replace ("[","")
+    a=a.replace ("]","")
+    с=a.find (sub_string)
+    if с==-1:
+        print ('Нет')
+    else: print ('Есть')
+def t5 (strings):
+    list = []
+    for element in strings:
+        if isinstance(element,int):
+            list.append(element)
+    import copy
+    new_list = copy.copy(list)
+    p=1
+    for i in range (len(new_list)-1):
+        p=new_list[i]*new_list[i-1]
+        new_list[i]=p
+    list.append (p)
+    print(list)

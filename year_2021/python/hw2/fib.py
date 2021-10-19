0c7d66c289ca44e69d0bcd6d1479a0b50260797c
alina1921 <92743460+alina1921@users.noreply.github.com>
2021-10-19 11:45:55 +0300

    Ivanova Alina

--- year_2021/python/hw2/fib.py
+++ year_2021/python/hw2/fib.py
@@ -1,20 +1,5 @@
-
 def fibonacci():
-    """
-    # Задание 4
-
-    Написать генератор чисел Фибоначчи
-
-    Input:
-    ```
-
-    ```
-
-    Output:
-    ```
-        next call: 0
-        next call: 1
-        ...
-    ```
-    """
-    pass
+    a, b = 0, 1
+    while 1:
+        yield a
+        a, b = b, a + b

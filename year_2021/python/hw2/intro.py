9a122756857bd462aebfc7ba908c3784497f666c
alina1921 <92743460+alina1921@users.noreply.github.com>
2021-10-19 11:46:56 +0300

    Ivanova Alina

--- year_2021/python/hw2/intro.py
+++ year_2021/python/hw2/intro.py
@@ -1,38 +1,32 @@
 from typing import List, Optional, Any
 
-
 def reverse(lst: Optional[List[Any]]):
     """
     Напишите функцию, котороя разворачивает список, используя срезы (индексацию элементов).
-
     Input:
     ```
         [1, 2, 3, 4]
     ```
-
     Oputput:
     ```
         [4, 3, 2, 1]
     ```
     """
-    return
-
-
-def filter_by_indices(lst: Optional[List[Any]], indices: Optional[List[Any]]):
+    a = str (lst [::-1])
+    print (a)   
+def filter_by_indices(lst, indices):
     """
     Напишите функцию, которая удаляет список индексов из списка.
     (
       Для удаления используется оператор `del`: `del my_list[1]` или `.pop()`
     )
-
     Input:
     ```
         [1, 2, 3, 4], [0, 1]
     ```
-
     Output:
     ```
         [3, 4]
     ```
     """
-    return
+    return [val for i, val in enumerate(lst) if i not in indices] 
